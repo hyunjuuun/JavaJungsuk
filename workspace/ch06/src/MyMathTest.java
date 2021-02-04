ee17f7105c6afa6cf521fc28179bcc114c1b5775
hyunjuuun <hyunjun86120@gmail.com>
2021-02-04 14:52:16 +0900

    add printGugudan method

--- workspace/ch06/src/MyMathTest.java
+++ workspace/ch06/src/MyMathTest.java
@@ -12,11 +12,22 @@ public class MyMathTest {
 		System.out.println("subtract(5L, 3L) = " + result2);
 		System.out.println("multiply(5L, 3L) = " + result3);
 		System.out.println("divide(5L, 3L) = " + result4);
+		mm.printGugudan(12);
 	}
 
 }
 
 class MyMath {
+	void printGugudan(int x) {
+		if (!(2 <= x && x <= 9)) {
+			System.out.println("출력 가능한 범위를 벗어났습니다.");
+			return;
+		}
+		for(int i=1; i<10; i++) {
+			System.out.printf("%d * %d = %d%n", x, i, x * i);
+		}
+		
+	}
 	long add(long a, long b) { return a+b ;}
 	long subtract(long a, long b) { return a-b; }
 	long multiply(long a, long b) { return a*b; }
