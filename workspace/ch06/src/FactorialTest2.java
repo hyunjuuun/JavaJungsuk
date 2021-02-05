c2e6fda4b6d99a8a0cb94217acbf87ad8db118dd
hyunjuuun <hyunjun86120@gmail.com>
2021-02-05 10:24:51 +0900

    add FactorialTest2

---
+++ workspace/ch06/src/FactorialTest2.java
@@ -0,0 +1,28 @@
+
+class FactorialTest2 {
+	
+	static long factorial(int n) {
+		if (n<=0 || n>20) return -1; // 매개변수의 유효성 검사
+		if (n<=1) return 1;
+		
+		return n * factorial(n-1);
+	}
+
+	public static void main(String[] args) {
+		int n = 21;
+		long result = 0;
+		
+		for(int i=1; i<=n; i++) {
+			result = factorial(i);
+			
+			if (result == -1) {
+				System.out.printf("유효하지 않은 값입니다. (0<n<=20) 조건을 맞춰주세요. 현재 입력: %d%n", n);
+				break;
+			}
+			
+			System.out.printf("%2d!=%20d%n", i, result);
+		}
+		
+	} 
+
+}
