90013db73224801a0f3f2b8bf83628d40955801f
hyunjuuun <hyunjun86120@gmail.com>
2021-02-11 22:06:26 +0900

    fix CarTest

--- workspace/ch06/src/CarTest.java
+++ workspace/ch06/src/CarTest.java
@@ -19,8 +19,8 @@ class CarTest {
 		c1.gearType = "auto";
 		c1.door = 4;
 		
-		Car c2 = new Car("white", "auto", 4);
-
+		Car c2 = new Car("white", "auto", 4); 
+		
 		System.out.println("c1의 color=" + c1.color + ", gearType=" + c1.gearType + ", door=" + c1.door);
 		System.out.println("c2의 color=" + c2.color + ", gearType=" + c2.gearType + ", door=" + c2.door);
 		
