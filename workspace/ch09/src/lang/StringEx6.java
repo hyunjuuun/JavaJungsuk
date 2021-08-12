247b62bcaf327a5c8c90aa1fb319c2d48d12d6ab
hyunjuuun <hyunjun86120@gmail.com>
2021-08-12 16:36:58 +0900

    TIL

---
+++ workspace/ch09/src/lang/StringEx6.java
@@ -0,0 +1,19 @@
+package lang;
+
+public class StringEx6 {
+	public static void main(String[] args) {
+		
+		int iVal = 100;
+		String strVal = String.valueOf(iVal); // "100"
+		
+		double dVal = 200.0;
+		String strVal2 = dVal + ""; // "200.0"
+		
+		double sum = Integer.parseInt("+"+strVal)+Double.parseDouble(strVal2);
+		double sum2 = Integer.valueOf(strVal) + Double.valueOf(strVal2);
+		
+		System.out.println(String.join("", strVal, "+", strVal2, "=")+sum);
+		System.out.println(strVal+"+"+strVal2+"="+sum2);
+		
+	}
+}
